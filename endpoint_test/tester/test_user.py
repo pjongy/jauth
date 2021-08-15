1bbd32c017ec9de3d4fb64faf7a0cab5649e06c8
pjongy <hi.pjongy@gmail.com>
2021-08-15 21:58:02 +0900

    Implement third party user test

--- endpoint_test/tester/test_user.py
+++ endpoint_test/tester/test_user.py
@@ -22,6 +22,7 @@ class TestUser(unittest.IsolatedAsyncioTestCase):
         assert user['email'] == dummy_email
         assert user['account'] == dummy_account
         assert user['type'] == UserType.EMAIL
+        assert 'third_party_user_id' not in user  # Only for third party users
 
     async def test_email_user_signup_failed_if_duplicated_account(self):
         dummy_email = 'dummy@user.com'
@@ -46,3 +47,58 @@ class TestUser(unittest.IsolatedAsyncioTestCase):
             'extra': {},
         })
         assert status == 409
+
+    async def test_third_party_user_signup_succeed(self):
+        # Tokens from /endpoint_test/jauth/application.py
+        dummy_users = {
+            'dummy-facebook-token': UserType.FACEBOOK,
+            'dummy-kakao-token': UserType.KAKAO,
+            'dummy-apple-token': UserType.APPLE,
+            'dummy-google-token': UserType.GOOGLE,
+        }
+        for token, user_type in dummy_users.items():
+            status, body, _ = await post(f'{TEST_ENDPOINT}/users/third_party', parameters={
+                'third_party_token': token,
+                'user_type': user_type,
+                'extra': {},
+            })
+            assert status == 200
+            user = body['result']
+            assert user['type'] == user_type
+            assert 'account' not in user  # Only for email users
+            assert 'third_party_user_id' in user
+
+    async def test_third_party_user_signup_failed_if_duplicated_third_party_id(self):
+        # Tokens from /endpoint_test/jauth/application.py
+        dummy_users = {
+            'dummy-facebook-token': UserType.FACEBOOK,
+            'dummy-kakao-token': UserType.KAKAO,
+            'dummy-apple-token': UserType.APPLE,
+            'dummy-google-token': UserType.GOOGLE,
+        }
+        for token, user_type in dummy_users.items():
+            status, body, _ = await post(f'{TEST_ENDPOINT}/users/third_party', parameters={
+                'third_party_token': token,
+                'user_type': user_type,
+                'extra': {},
+            })
+            assert status == 200
+            user = body['result']
+            assert user['type'] == user_type
+            assert 'account' not in user  # Only for email users
+            assert 'third_party_user_id' in user
+
+        # Tokens from /endpoint_test/jauth/application.py
+        dummy_users = {
+            'dummy-facebook-token-dup': UserType.FACEBOOK,
+            'dummy-kakao-token-dup': UserType.KAKAO,
+            'dummy-apple-token-dup': UserType.APPLE,
+            'dummy-google-token-dup': UserType.GOOGLE,
+        }
+        for token, user_type in dummy_users.items():
+            status, body, _ = await post(f'{TEST_ENDPOINT}/users/third_party', parameters={
+                'third_party_token': token,
+                'user_type': user_type,
+                'extra': {},
+            })
+            assert status == 409
